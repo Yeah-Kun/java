d8304948f0d22c44f9a85c6d151a96e5a9ff2ffb
Yeah-Kun <767786685@qq.com>
2017-07-07 23:46:16 +0800

    update

--- production_practice_project/Library/mainlyModule/library/view/SimpleTableDemo.java
+++ production_practice_project/Library/mainlyModule/library/view/SimpleTableDemo.java
@@ -1,15 +1,17 @@
-/**日13月751
- * 2017年7:14:
+/**
+ * 2017年7月7日23:45:16
  * 简单表格，用于测试窗口容器
  */
 package library.view;
 
+import javax.persistence.Table;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.TableColumn;
 
 public class SimpleTableDemo {
 	
-	// 表格的列名
+	// 创建一个简单的表格,以下是表格的列名
 	String[] columnNames = { "first name", "last name", "sport", "# of Years",
 			"Vegetarian" };
 	// 表格的具体数据
@@ -21,7 +23,22 @@ public class SimpleTableDemo {
 			{ "john", "Doe", "Rowing", new Integer(3), new Boolean(true) },
 			{ "Joe", "Brown", "Pool", new Integer(10), new Boolean(false) } };
 
-	JTable table = new JTable(data, columnNames);  
-	JScroll
-	Pane scrollPane = new JScrollPane(table);  
+	// 向容器添加表格
+	JTable table = new JTable(data, columnNames);  // 接收数据
+	JScrollPane scrollPane = new JScrollPane(table); // 创建一个容纳table的容器
+//	table.setFillsViewportHeight(true);  // 占据容器整个高度
+	
+	//设置每一列的宽度
+	TableColumn column = new TableColumn();{
+		for (int i = 0; i < 5; i++) {  
+			column = table.getColumnModel().getColumn(i);  
+		    if (i == 2) {  
+		        column.setPreferredWidth(100); //third column is bigger  
+		    } else {  
+		        column.setPreferredWidth(50);  
+		    }  
+		} 
+	} 
+	
+	
 }
