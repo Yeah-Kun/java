a2be0f357ccfded223fc07dfe6b770a2644c7b8f
Yeah-Kun <767786685@qq.com>
2017-07-08 12:49:48 +0800

    update

--- production_practice_project/Library/mainlyModule/library/dao/BookInterface.java
+++ production_practice_project/Library/mainlyModule/library/dao/BookInterface.java
@@ -4,9 +4,12 @@
  */
 package library.dao;
 
+import java.util.ArrayList;
+
 import library.model.Book;
 
 public interface BookInterface {
+	public ArrayList<Book> FindAll(); // 查找所有图书的信息
 	public Book FindbyID(int id); // 通过ID查找press表里面的内容
 	public int insert(Book entity);// 添加一个实体，顺序为：name、adress、url、mail
 	public int delete(Book enity); //删除
--- production_practice_project/Library/mainlyModule/library/dao/GetBook.java
+++ production_practice_project/Library/mainlyModule/library/dao/GetBook.java
@@ -11,6 +11,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import library.model.Book;
+import library.model.Press;
 
 public class GetBook implements BookInterface{
 	
@@ -18,13 +19,12 @@ public class GetBook implements BookInterface{
 	ResultSet result = null; // 用来保存数据库的二维表信息
 	PreparedStatement ps = null; // PreparedStatement对象，用于存储Connection对象返回的内容
 	Book book = null;
+	GetCategory gc = null;
+	GetPress gp = null;
 	
 	public ArrayList<Book> FindAll(){ // 由于查找到的图书信息可能不止一条，所以用一个列表存储
 		ArrayList<Book> bookList = new ArrayList<>();
-		Book book = null;
-		GetCategory gc = null;
-		GetPress gp = null;
-		
+
 		String sql = "select * from book";
 		try {
 			ps = conn.prepareStatement(sql);
@@ -48,14 +48,56 @@ public class GetBook implements BookInterface{
 	}
 	@Override
 	public Book FindbyID(int id) {
+		Book book = null;
+		String sql = "select * from book where id=?"; // 使用sql语句查询数据库
+		try {
+			// 访问数据库 通过ID，获取一条数据
+			ps = conn.prepareStatement(sql); 
+			ps.setInt(1, id);
+			
+			result = ps.executeQuery(); // 得到数据库信息，并保存到result
+			
+			// 执行以下语句，说明id存在
+			// 将result的信息存储到book对象里面
+			if (result.next()) {
+				book = new Book();
+				gp = new GetPress();
+				gc = new GetCategory();
+				// 将result的二维表内容放到press对象里面
+				book.setId(result.getInt("id")); // 从result中获取id
+				book.setISBN(result.getString("ISBN"));
+				book.setName(result.getString("name"));
+				book.setPrice(result.getDouble("price"));
+				book.setPress(gp.FindbyID(result.getInt("pressID")));
+				book.setCategory(gc.FindbyID(result.getInt("categoryID")));
+			}
+		} catch (SQLException e) {
+			//System.out.println("数据库提取信息错误！");
+			e.printStackTrace();
+		}
 
-		return null;
+		return book; // 如果获取失败则为null
+
 	}
 
 	@Override
 	public int insert(Book entity) {
-		// TODO 自动生成的方法存根
-		return 0;
+		int re=0;
+		String sql = "INSERT INTO Press(name,ISBN,price,pressID,categoryID) VALUES(?,?,?,?,?)"; // 使用SQL语句查询数据库
+		try {
+			// 访问数据库，增加一条完整的信息
+			ps = conn.prepareStatement(sql); // 跟mysql建立连接
+			ps.setString(1, entity.getName());
+			ps.setString(2, entity.getISBN());
+			ps.setDouble(3, entity.getPrice());
+			ps.setString(4, entity.getPress());
+			re = ps.executeUpdate(); // 执行，将结果返回
+
+		} catch (SQLException e) {
+			// System.out.println("数据库插入信息错误！");
+			e.printStackTrace();
+		}
+		return re;
 	}
 
 	@Override
